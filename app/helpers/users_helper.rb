c648dff456adca0809c8d9bcab2a376efd90cc6d
Lukas Hauser <lukashauser@lukass-air-2.mynet>
2020-12-12 20:34:07 +0000

    updated gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.firstname, class: "gravatar" )
+    image_tag(gravatar_url, alt: user.firstname, class: "gravatar rounded-circle" )
   end
 end
