14f0a9a57d731c6367054099fbb5678333276e4b
Lukas Hauser <lukashauser@lukass-air-2.mynet>
2021-03-23 23:46:06 +0000

    Update forms

--- app/controllers/html_pages_controller.rb
+++ app/controllers/html_pages_controller.rb
@@ -1,7 +1,7 @@
 class HtmlPagesController < ApplicationController
   def home
     if logged_in?
-      @space_feed_items = (current_user.space_feed).paginate(page: params[:page])
+      @space_feed_items = (current_user.space_feed).where(active:true).paginate(page: params[:page])
       @art_feed_items = (current_user.art_feed).paginate(page: params[:page])
     end
   end
