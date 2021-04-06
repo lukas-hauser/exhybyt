e38e110f854c7ee5f21e98a9d8e32e33eb34da9a
Lukas Hauser <lukashauser@lukass-air-2.home>
2021-04-06 17:24:38 +0100

    update status after successful payment intent

--- app/services/stripe_payment_intent_service.rb
+++ app/services/stripe_payment_intent_service.rb
@@ -1,6 +1,6 @@
 class StripePaymentIntentService
   def call(event)
-    reservation = Reservation.find_by(payment_intent_id: event.data.object.id)
-    reservation.update(payment_captured: true) if event.data.object.payment_intent.succeeded?
+    reservation = Reservation.find_by(payment_intent_id: event.data.object.payment_intent.succeeded)
+    reservation.update(payment_captured: true)
   end
 end
