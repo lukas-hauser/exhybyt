1e83ffafb6ee24af9dc0703cd678def63c5ee3db
Lukas Hauser <lukashauser@lukass-air-2.home>
2021-04-12 23:11:59 +0100

    fixed order webhook bug

--- app/services/stripe_payment_intent_service.rb
+++ app/services/stripe_payment_intent_service.rb
@@ -4,9 +4,9 @@ class StripePaymentIntentService
     if !reservation.nil?
       reservation.update(payment_captured: true,
       status: "Confirmed and paid")
-    else
-      order = Order.find_by(payment_intent_id: event.data.object.id)
-      order.update(status: "Payment Captured")
+#    else # There is currently no Payment Intent ID for Orders
+#      order = Order.find_by(payment_intent_id: event.data.object.id)
+#      order.update(status: "Payment Captured")
     end
   end
 end
